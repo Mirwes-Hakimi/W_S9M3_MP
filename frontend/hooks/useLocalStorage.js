287444b1ffc9006850f6c5fb164a62994648261e
Mirwes Hakimi <mirwes215@gmail.com>
2025-04-03 19:50:37 -0700

    created useLocalStorage function that saves recent UI updates to the browser

--- frontend/hooks/useLocalStorage.js
+++ frontend/hooks/useLocalStorage.js
@@ -1,6 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 
-export default function useLocalStorage(key, initialValue) {
-   
+export default function useLocalStorage(key, value) {
+    const [v, setV] = useState(() => {
+        const initial = localStorage.getItem(key)
+          return initial ? JSON.parse(initial) : value
+    })
+
+    const setValueLS = value => {
+        setV(value)
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+    return [v, setValueLS]
 }
